d2b3ff28e8a23e443e67afce654e046c8d08bc7a
ALittleDaisy <1152594956@qq.com>
2023-11-10 11:19:08 +0800

    获取剪切板的内容

--- utilsgather/src/main/java/com/example/utilsgather/clipboard/ClipboardUtil.java
+++ utilsgather/src/main/java/com/example/utilsgather/clipboard/ClipboardUtil.java
@@ -6,6 +6,21 @@ import android.content.Context;
 
 public class ClipboardUtil {
     /**
+     * 获取剪切板上的内容
+     */
+    public static String getClipboardContent(Context context) {
+        ClipboardManager cm = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData data = cm.getPrimaryClip();
+        if (data != null && data.getItemCount() > 0) {
+            ClipData.Item item = data.getItemAt(0);
+            if (item != null) {
+                return item.coerceToText(context).toString();
+            }
+        }
+        return null;
+    }
+
+    /**
      * 将内容复制到剪切板
      */
     public static void copyToClipboard(String copyStr, Context context) {
