0ea3d64c483d3d15008914f953d25a8b7a9b5e40
ALittleDaisy <1152594956@qq.com>
2023-11-16 16:23:29 +0800

    1.增加新模块UiOperator 2.utilsgather中封装一个新的功能用于快速引导的

---
+++ uioperate/src/main/java/com/example/uitraining/UiOperateEntranceActivity.java
@@ -0,0 +1,15 @@
+package com.example.uitraining;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UiOperateEntranceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_operate_entrance);
+
+    }
+}
