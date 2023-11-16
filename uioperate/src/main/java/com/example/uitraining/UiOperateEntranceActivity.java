00d1bb6fc91ac159900d81466e444554eff2a636
ALittleDaisy <1152594956@qq.com>
2023-11-16 18:36:02 +0800

    uioperate增加一个选项

--- uioperate/src/main/java/com/example/uitraining/UiOperateEntranceActivity.java
+++ uioperate/src/main/java/com/example/uitraining/UiOperateEntranceActivity.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.utilsgather.list_guide.GuideItemEntity;
+import com.example.utilsgather.list_guide.GuideSettings;
+
 public class UiOperateEntranceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui_operate_entrance);
+
+        GuideSettings.set(findViewById(R.id.uioperate_lv_launcher), new GuideItemEntity[]{
+                new GuideItemEntity("替换ViewStud", new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                })
+        });
 
     }
 }
