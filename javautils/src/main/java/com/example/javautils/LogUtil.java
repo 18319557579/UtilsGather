286e0e0fe7e849b839a2ed5162bc85b5619e6a18
ALittleDaisy <1152594956@qq.com>
2023-12-21 19:38:12 +0800

    增加一个正则方面的函数：是否以指定字符串开头

---
+++ javautils/src/main/java/com/example/javautils/LogUtil.java
@@ -0,0 +1,7 @@
+package com.example.javautils;
+
+public class LogUtil {
+    public static void println(String msg) {
+        System.out.println("Daisy " + msg);
+    }
+}
--- javautils/src/main/java/com/example/javautils/regex/RegexPackaged.java
+++ javautils/src/main/java/com/example/javautils/regex/RegexPackaged.java
@@ -1,5 +1,8 @@
 package com.example.javautils.regex;
 
+import com.example.javautils.LogUtil;
+
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexPackaged {
@@ -17,5 +20,26 @@ public class RegexPackaged {
      */
     public static String getFileName(String wholeFileName) {
         return Pattern.compile("[.][^.]*$").matcher(wholeFileName).replaceFirst("");
+    }
+
+    /**
+     * 判断inputStr是否以prefix中的某个开头
+     * 例，(^http://)|(^https://)
+     * 转义之类的东西没有考虑
+     */
+    public static boolean startOf(String inputStr, String... prefix) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String pre: prefix) {
+            if (!stringBuilder.toString().isEmpty()) {
+                stringBuilder.append("|");
+            }
+            String subschema = String.format("(^%s)", pre);
+            stringBuilder.append(subschema);
+        }
+        LogUtil.println("得到的regex:" + stringBuilder.toString());
+
+        Pattern pattern = Pattern.compile(stringBuilder.toString());
+        Matcher matcher = pattern.matcher(inputStr);
+        return matcher.find();
     }
 }
