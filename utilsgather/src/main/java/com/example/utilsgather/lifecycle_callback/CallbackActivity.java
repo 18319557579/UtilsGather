74dffcb8da10d2ec5631bc84bad8f1e1163140af
yezhaochu <yezhaochu@ichestnuts.com>
2023-12-28 16:21:56 +0800

    将日志打印统一改为使用LogUtil

--- utilsgather/src/main/java/com/example/utilsgather/lifecycle_callback/CallbackActivity.java
+++ utilsgather/src/main/java/com/example/utilsgather/lifecycle_callback/CallbackActivity.java
@@ -2,10 +2,11 @@ package com.example.utilsgather.lifecycle_callback;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.utilsgather.logcat.LogUtil;
 
 public class CallbackActivity extends AppCompatActivity {
     private final String canonicalName = getClass().getCanonicalName();
@@ -13,48 +14,48 @@ public class CallbackActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Daisy", canonicalName + " 回调onCreate");
+        LogUtil.d("Daisy", canonicalName + " 回调onCreate");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Daisy", canonicalName + " 回调onResume");
+        LogUtil.d("Daisy", canonicalName + " 回调onResume");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Daisy", canonicalName + " 回调onStart");
+        LogUtil.d("Daisy", canonicalName + " 回调onStart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("Daisy", canonicalName + " 回调onPause");
+        LogUtil.d("Daisy", canonicalName + " 回调onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("Daisy", canonicalName + " 回调onStop");
+        LogUtil.d("Daisy", canonicalName + " 回调onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("Daisy", canonicalName + " 回调onDestroy");
+        LogUtil.d("Daisy", canonicalName + " 回调onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("Daisy", canonicalName + " 回调onRestart");
+        LogUtil.d("Daisy", canonicalName + " 回调onRestart");
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        Log.d("Daisy", canonicalName + " 回调onNewIntent");
+        LogUtil.d("Daisy", canonicalName + " 回调onNewIntent");
     }
 }
