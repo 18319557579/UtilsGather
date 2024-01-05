c5577a608b8328dd40c0f1f9d53e332b34ccc67d
yezhaochu <yezhaochu@ichestnuts.com>
2024-01-05 15:50:03 +0800

    系统文本分享完善

--- utilsgather/src/main/java/com/example/utilsgather/share/SystemShareUtil.java
+++ utilsgather/src/main/java/com/example/utilsgather/share/SystemShareUtil.java
@@ -5,13 +5,24 @@ import android.content.Intent;
 
 public class SystemShareUtil {
     /**
-     * 系统文本分享
+     * 系统文本分享. 可以带标题
      */
-    public static void textShare(Activity activity, String shareContent) {
+    public static void textShare(Activity activity, String shareContent, String title) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
         intent.putExtra(Intent.EXTRA_TEXT, shareContent);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享");  //效果未知
+        if (title != null) {
+            intent.putExtra(Intent.EXTRA_TITLE, title);  //可以看到标题
+        }
         intent.setType("text/plain");
-        activity.startActivity(intent);
+        activity.startActivity(Intent.createChooser(intent, "分享"));  //效果未知, "分享"这两个字在findx5pro上也出不来
+    }
+
+    /**
+     * 系统文本分享,不带标题
+     */
+    public static void textShare(Activity activity, String shareContent) {
+        textShare(activity, shareContent, null);
     }
 }
