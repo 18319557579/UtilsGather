b730c0b57bbeac1b6a415f644ea444e81c82d344
ALittleDaisy <1152594956@qq.com>
2024-01-16 10:58:17 +0800

    base64编解码

--- utilsgather/src/main/java/com/example/utilsgather/encoding/Base64Util.java
+++ utilsgather/src/main/java/com/example/utilsgather/encoding/Base64Util.java
@@ -3,13 +3,25 @@ package com.example.utilsgather.encoding;
 
 import android.util.Base64;
 
+import java.nio.charset.Charset;
+
 public class Base64Util {
+    /**
+     * 将String进行Base64编码
+     */
+    public static String base64Encode(String content) {
+        byte[] contentByte = content.getBytes(Charset.forName("UTF-8"));
+        return Base64.encodeToString(contentByte, Base64.DEFAULT);
+    }
+
     /**
      * 将Base64的数据进行解码后返回
      */
-    public static String decodeBase64(String originStr) {
+    public static String base64Decode(String originStr) {
         if (originStr == null) return null;
+
         byte[] b = Base64.decode(originStr, Base64.DEFAULT);
-        return new String(b);
+        return new String(b, Charset.forName("UTF-8"));
     }
+
 }
