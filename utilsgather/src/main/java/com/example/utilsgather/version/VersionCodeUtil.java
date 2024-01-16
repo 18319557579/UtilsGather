8dce5f53a11861b4d86b382bc7a470ba64991146
yezhaochu <yezhaochu@ichestnuts.com>
2024-01-16 16:20:02 +0800

    获得版本号

--- utilsgather/src/main/java/com/example/utilsgather/version/VersionCodeUtil.java
+++ utilsgather/src/main/java/com/example/utilsgather/version/VersionCodeUtil.java
@@ -1,5 +1,8 @@
 package com.example.utilsgather.version;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
 public class VersionCodeUtil {
@@ -8,5 +11,19 @@ public class VersionCodeUtil {
      */
     public static boolean versionReached(int targetVersion) {
         return Build.VERSION.SDK_INT >= targetVersion;
+    }
+
+    /**
+     * 获得版本号
+     */
+    public static int getPackageVersionCode(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
