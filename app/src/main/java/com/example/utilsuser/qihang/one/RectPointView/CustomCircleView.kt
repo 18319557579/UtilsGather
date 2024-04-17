1924cd0bb552fe7feffd8b9adea4cf2202bae1d1
ALittleDaisy <1152594956@qq.com>
2024-04-17 14:37:34 +0800

    作圆

--- app/src/main/java/com/example/utilsuser/qihang/one/RectPointView/CustomCircleView.kt
+++ app/src/main/java/com/example/utilsuser/qihang/one/RectPointView/CustomCircleView.kt
@@ -3,10 +3,12 @@ package com.example.utilsuser.qihang.one.RectPointView
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+import com.example.utilsuser.R
 
 class CustomCircleView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -17,7 +19,21 @@ class CustomCircleView @JvmOverloads constructor(
     private lateinit var mPath: Path
 
     init {
-//        mBmp = BitmapFactory.decodeResource(resources, R.drawable.avator)
+        mBmp = BitmapFactory.decodeResource(resources, R.drawable.avator)
+        mPaint = Paint()
+        mPath = Path()
+        val width = mBmp.width
+        val height = mBmp.height
+        mPath.addCircle(width / 2f, height / 2f, width / 2f, Path.Direction.CCW)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.save()
+        canvas.clipPath(mPath)
+        canvas.drawBitmap(mBmp, 0f, 0f, mPaint)
+        canvas.restore()
     }
 
 }
