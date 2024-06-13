58198d2369e814374116f7b24dbb56ee873da3cd
ALittleDaisy <1152594956@qq.com>
2024-06-13 14:45:22 +0800

    在初始进入Activity时的地方，将异步切换的代码改成RxJava

--- app/src/main/java/com/example/utilsuser/file/list/database/DownloadTaskActivity.java
+++ app/src/main/java/com/example/utilsuser/file/list/database/DownloadTaskActivity.java
@@ -24,8 +24,12 @@ import com.example.utilsuser.file.list.database.thread.Scheduler;
 import java.io.File;
 import java.util.List;
 
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 //todo 退出这个Activity时，要关闭哪些东西 1
-//todo 自行封装RxJava 1
+//todo 自行封装RxJava 1 -> 变成引入Rxjava了 1
 //todo 这个Activity的线程切换是不是可以使用Handler来进行优化
 //todo 优化Database，是不是可以让SQLiteDatabase对象变成单例模式
 //todo Activity去notify Adapter的时候，总是要找id所在的index，有没有办法节约这个步骤呢
@@ -87,34 +91,28 @@ public class DownloadTaskActivity extends AppCompatActivity {
         });
 
         rv.setAdapter(downloaTaskAdapter);
-
     }
 
     private void loadData() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                downloadTaskBeans = DownloadTaskDao.newInstance().queryTaskList();
-                for (DownloadTaskBean downloadTaskBean : downloadTaskBeans) {
-                    File file = new File(downloadTaskBean.getPath());
-                    if (file.exists()) {
-                        downloadTaskBean.setCurrentLength(file.length());
-                    } else {
-                        downloadTaskBean.setCurrentLength(-1);
+        Completable.fromAction(() -> {
+                    downloadTaskBeans = DownloadTaskDao.newInstance().queryTaskList();
+                    for (DownloadTaskBean downloadTaskBean : downloadTaskBeans) {
+                        File file = new File(downloadTaskBean.getPath());
+                        if (file.exists()) {
+                            downloadTaskBean.setCurrentLength(file.length());
+                        } else {
+                            downloadTaskBean.setCurrentLength(-1);
+                        }
                     }
-                }
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        initAdapter();
-                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                    initAdapter();
                 });
-            }
-        }).start();
     }
 
-    private void registerBroadCast(){
+    private void registerBroadCast() {
         changeReceiver = new ChangeReceiver(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -148,19 +146,24 @@ public class DownloadTaskActivity extends AppCompatActivity {
                 "110_54c27c8bd85e0b3f44e7d5491b5aa6a0.apk",
                 "记账本");
     }
+
     public void downloadDouYin(View view) {
         addTaskToStart("https://ucdl.25pp.com/fs08/2024/05/28/8/120_1520704cba97e878685c716c5dd89961.apk?nrd=0&fname=%E6%8A%96%E9%9F%B3&productid=2011&packageid=500960169&pkg=com.ss.android.ugc.aweme&vcode=300101&yingid=wdj_web&pos=wdj_web%2Fdetail_normal_dl%2F0&appid=7461948&apprd=7461948&iconUrl=http%3A%2F%2Fandroid-artworks.25pp.com%2Ffs08%2F2024%2F05%2F29%2F0%2F110_948e6c5440768f9a40ddaf6036045056_con.png&did=6499240c97db2fab01ff289f059dbb92&md5=17dded2bed52cb677eb31ab8f3df33b6",
                 "/data/user/0/com.example.utilsuser/files/",
                 "120_1520704cba97e878685c716c5dd89961.apk",
                 "抖音");
     }
+
     public void downloadWeiXin(View view) {
     }
+
     public void downloadXiaoHongShu(View view) {
     }
+
     public void downloadPiPiXia(View view) {
 
     }
+
     public void showInfo(View view) {
         StringBuilder stringBuilder = new StringBuilder();
         for (DownloadTaskBean downloadTaskBean : downloadTaskBeans) {
@@ -205,15 +208,19 @@ public class DownloadTaskActivity extends AppCompatActivity {
     public void notifyAdd(DownloadTaskBean downloadTaskBean) {
         downloaTaskAdapter.notifyAdd(downloadTaskBean);
     }
+
     public void notifyPause(int id) {
         downloaTaskAdapter.notifyPause(id);
     }
+
     public void notifyFinished(int id) {
         downloaTaskAdapter.notifyFinished(id);
     }
+
     public void notifyCleared(int id) {
         downloaTaskAdapter.notifyCleared(id);
     }
+
     public void notifyError(int id, int errorCode) {
         switch (errorCode) {
             case 0:
@@ -230,9 +237,10 @@ public class DownloadTaskActivity extends AppCompatActivity {
     public void pauseTask(int id) {
         downloadBinder.pauseTask(id);
     }
+
     public void resumeTask(DownloadTaskBean downloadTaskBean) {
         Scheduler.runOnBGThread(() -> {
-            if (! new File(downloadTaskBean.getPath()).exists()) {
+            if (!new File(downloadTaskBean.getPath()).exists()) {
                 DownloadTaskDao.newInstance().updateTask(downloadTaskBean.getId(), -1L);
                 downloadTaskBean.setCurrentLength(-1L);
             }
@@ -241,6 +249,7 @@ public class DownloadTaskActivity extends AppCompatActivity {
             });
         });
     }
+
     public void clearTask(DownloadTaskBean downloadTaskBean, boolean inExecutor) {
         if (inExecutor) {
             downloadBinder.clearTask(downloadTaskBean.getId());
