f319ff9764c47f1227aba87bd67168dfe5539678
ALittleDaisy <1152594956@qq.com>
2024-07-01 12:25:09 +0800

    简单地打开了图片选择

--- uioperate/src/main/java/com/example/uioperate/picture_selection/PictureSelectionActivity.kt
+++ uioperate/src/main/java/com/example/uioperate/picture_selection/PictureSelectionActivity.kt
@@ -1,16 +1,19 @@
 package com.example.uioperate.picture_selection
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.uioperate.R
 import com.example.uioperate.databinding.ActivityPictureSelectionBinding
-import com.example.uioperate.databinding.ActivityUiOperateEntranceBinding
 import com.example.utilsgather.list_guide.GuideItemEntity
 import com.example.utilsgather.list_guide.GuideSettings
+import com.example.utilsgather.logcat.LogUtil
+
 
 class PictureSelectionActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityPictureSelectionBinding
+
+    private val REQEUST_COE = 8001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,35 @@ class PictureSelectionActivity : AppCompatActivity() {
 
         GuideSettings.set(mBinding.lvPicSelectionOperation, arrayOf(
             GuideItemEntity("单选图片") {
+                /*val intentChooser = Intent().apply {
+                    action = Intent.ACTION_GET_CONTENT
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    putExtras(Bundle().apply {
+                        putStringArray(Intent.EXTRA_MIME_TYPES, arrayOf("image/gif", "image/jpeg", "image/webp", "image/png", "image/bmp"))
+                        putBoolean(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                    })
+                }
+                startActivityForResult(intentChooser, 9001);*/
+
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.setType("image/*")
+                startActivityForResult(intent, REQEUST_COE)
             },
         ))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            REQEUST_COE -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    LogUtil.d("图片选择成功")
+
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    LogUtil.d("图片选择失败")
+                }
+            }
+        }
     }
 }
