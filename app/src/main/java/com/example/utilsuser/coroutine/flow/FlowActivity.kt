af565930466f85fd18e649a5bf4d656afebf8b39
ALittleDaisy <1152594956@qq.com>
2024-07-04 11:54:32 +0800

    研究了一手Flow的声明周期管理

--- app/src/main/java/com/example/utilsuser/coroutine/flow/FlowActivity.kt
+++ app/src/main/java/com/example/utilsuser/coroutine/flow/FlowActivity.kt
@@ -1,5 +1,6 @@
 package com.example.utilsuser.coroutine.flow
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -8,12 +9,18 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.utilsgather.logcat.LogUtil
 import com.example.utilsuser.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.utilsgather.list_guide.GuideItemEntity
+import com.example.utilsgather.list_guide.GuideSettings
+import com.example.utilsuser.coroutine.flow_operator.FlowOperatorActivity
+
 
 class FlowActivity : AppCompatActivity() {
     private val flowActivityViewModel by viewModels<FlowActivityViewModel>()
@@ -23,28 +30,49 @@ class FlowActivity : AppCompatActivity() {
         setContentView(R.layout.activity_flow)
 
         val textView = findViewById<TextView>(R.id.text_view)
-        val button = findViewById<Button>(R.id.button)
-        val button2 = findViewById<Button>(R.id.button2)
 
-        button.setOnClickListener {
-            lifecycleScope.launch {
-                flowActivityViewModel.timeFlow.collect { time ->
-                    textView.text = time.toString()
-                }
-                //当timeFlow的流执行完成后，才可以执行下面的代码。在此案例中由于timeFlow是死循环，因此下面的代码始终无法被执行
-                LogUtil.d("这里能够执行吗")
-            }
-        }
+        GuideSettings.set(findViewById(R.id.lv_launcher),
+            arrayOf(
+                GuideItemEntity("Start") {
+                    lifecycleScope.launch {
+                        flowActivityViewModel.timeFlow.collect { time ->
+                            textView.text = time.toString()
+                            LogUtil.d("Update time $time in UI.")
+                        }
 
-        button2.setOnClickListener {
-            lifecycleScope.launch {
-                //collectLatest剩下未处理完的逻辑不处理了，直接接收新值重新运行处理逻辑，所以有新数据来的时候，delay(3000)将不会受影响
-                flowActivityViewModel.timeFlow.collectLatest  { time ->
-                    textView.text = time.toString()
-                    delay(3000)
-                }
-            }
-        }
+                        //当timeFlow的流执行完成后，才可以执行下面的代码。在此案例中由于timeFlow是死循环，因此下面的代码始终无法被执行
+                        LogUtil.d("这里能够执行吗")
+                    }
+                },
+                GuideItemEntity("Start（流速不均匀）") {
+                    lifecycleScope.launch {
+                        //collectLatest剩下未处理完的逻辑不处理了，直接接收新值重新运行处理逻辑，所以有新数据来的时候，delay(3000)将不会受影响
+                        flowActivityViewModel.timeFlow.collectLatest  { time ->
+                            textView.text = time.toString()
+                            delay(3000)
+                        }
+                    }
+                },
+                GuideItemEntity("使用launchWhenStarted") {
+                    lifecycleScope.launchWhenStarted {
+                        flowActivityViewModel.timeFlow.collect { time ->
+                            textView.text = time.toString()
+                            LogUtil.d("Update time $time in UI.")
+                        }
+                    }
+                },
+                GuideItemEntity("使用 repeatOnLifecycle (Lifecycle.State.STARTED)") {
+                    lifecycleScope.launch {
+                        repeatOnLifecycle (Lifecycle.State.STARTED) {
+                            flowActivityViewModel.timeFlow.collect { time ->
+                                textView.text = time.toString()
+                                LogUtil.d("Update time $time in UI.")
+                            }
+                        }
+                    }
+                },
+            )
+        )
 
     }
 }
--- app/src/main/java/com/example/utilsuser/coroutine/flow/FlowActivityViewModel.kt
+++ app/src/main/java/com/example/utilsuser/coroutine/flow/FlowActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.utilsuser.coroutine.flow
 
 import androidx.lifecycle.ViewModel
+import com.example.utilsgather.logcat.LogUtil
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
@@ -8,6 +9,7 @@ class FlowActivityViewModel : ViewModel() {
     val timeFlow = flow {
         var time = 0
         while (true) {
+            LogUtil.d("循环继续执行: $time")
             emit(time)
             delay(1000)
             time++
