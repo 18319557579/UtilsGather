7478969359f865f76d3107c79763329635015998
ALittleDaisy <1152594956@qq.com>
2024-07-16 18:02:59 +0800

    增加SAF的注释

--- uioperate/src/main/java/com/example/uioperate/storage/StorageActivity.kt
+++ uioperate/src/main/java/com/example/uioperate/storage/StorageActivity.kt
@@ -332,7 +332,8 @@ class StorageActivity : AppCompatActivity() {
                     }
                 },
 
-                //这个不需要存储权限
+                //通过SAF并不能直接拿到图片的路径，图片的信息封装在Uri里
+                //通过SAF不需要存储权限
                 GuideItemEntity("共享存储空间-媒体文件-通过SAF访问: 选择一张图片") {
                     val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
                         addCategory(Intent.CATEGORY_OPENABLE)
