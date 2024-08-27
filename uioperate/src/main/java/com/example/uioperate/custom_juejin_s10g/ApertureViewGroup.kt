2dc075fc22673b4ac6ba125be0f5bf794bea1d00
ALittleDaisy <1152594956@qq.com>
2024-08-27 15:59:16 +0800

    跑马灯初步改造成ViewGroup的形式

---
+++ uioperate/src/main/java/com/example/uioperate/custom_juejin_s10g/ApertureViewGroup.kt
@@ -0,0 +1,126 @@
+package com.example.uioperate.custom_juejin_s10g
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Outline
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+import android.graphics.Shader
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.view.animation.LinearInterpolator
+import android.widget.LinearLayout
+import androidx.core.graphics.withSave
+
+class ApertureViewGroup @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr){
+
+    companion object {
+        val DEF_WIDTH = 200.dp
+        val DEF_HEIGHT = DEF_WIDTH
+        private val RADIUS = 20.dp
+        private val INNER_BORER = 30.dp
+    }
+
+    private val animator by lazy {
+        // 改变字段 currentSpeed 的值
+        ObjectAnimator.ofFloat(this, "currentSpeed", 0f, 360f).apply {
+            repeatCount = ValueAnimator.INFINITE
+            duration = 2000L
+            interpolator = LinearInterpolator()  // 匀速旋转
+        }
+    }
+
+    // 使用setter，来多写一步操作 invalidate()
+    private var currentSpeed = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private val path by lazy {
+        Path().also {
+            it.addRoundRect(rectF, RADIUS, RADIUS, Path.Direction.CCW)
+        }
+    }
+
+    private val color1 by lazy {
+        LinearGradient(
+            width * 1f,
+            height / 2f,
+            width * 1f,
+            height * 1f,
+            intArrayOf(Color.TRANSPARENT, Color.RED),
+            floatArrayOf(0f, 1f),
+            Shader.TileMode.CLAMP
+        )
+    }
+    private val color2 by lazy {
+        LinearGradient(
+            width / 2f,
+            height / 2f,
+            width / 2f,
+            0f,
+            intArrayOf(Color.TRANSPARENT, Color.GREEN),
+            floatArrayOf(0f, 1f),
+            Shader.TileMode.CLAMP
+        )
+    }
+
+    init {
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                outline.setRoundRect(0, 0, view.width, view.height, RADIUS)
+            }
+        }
+        clipToOutline = true
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        animator.start()
+    }
+
+    // 内矩形
+    private val rectF by lazy {
+        val left = 0f + INNER_BORER / 2f
+        val top = 0f + INNER_BORER / 2f
+        val right = left + DEF_WIDTH - INNER_BORER
+        val bottom = top + DEF_HEIGHT - INNER_BORER
+        RectF(left, top, right, bottom)
+    }
+
+    override fun dispatchDraw(canvas: Canvas) {
+        canvas.withSave {
+            // 剪切后保留外面的
+            canvas.clipOutPath(path)
+
+            canvas.rotate(currentSpeed, width / 2f, height / 2f)
+
+            val left = rectF.left + rectF.width() / 2f
+            val right = rectF.right + rectF.width() / 2
+            val top = rectF.top + rectF.height() / 2f
+            val bottom = rectF.bottom + rectF.height() / 2f
+            // 绘制渐变view1
+            paint.shader = color1
+            canvas.drawRect(left, top, right, bottom, paint)
+            paint.shader = null
+
+            // 绘制渐变view2
+            paint.shader = color2
+            canvas.drawRect(left, top, -right, -bottom, paint)
+            paint.shader = null
+        }
+
+        super.dispatchDraw(canvas)
+    }
+}
