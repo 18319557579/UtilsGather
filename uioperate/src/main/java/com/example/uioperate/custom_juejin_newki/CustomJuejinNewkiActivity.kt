c2fd5188e742b7a83e5f56d0c37b65b7ad950313
ALittleDaisy <1152594956@qq.com>
2024-08-28 17:28:09 +0800

    仿原博主的搞定了

--- uioperate/src/main/java/com/example/uioperate/custom_juejin_newki/CustomJuejinNewkiActivity.kt
+++ uioperate/src/main/java/com/example/uioperate/custom_juejin_newki/CustomJuejinNewkiActivity.kt
@@ -2,27 +2,25 @@ package com.example.uioperate.custom_juejin_newki
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.uioperate.R
 import com.example.utilsgather.list_guide.GuideItemEntity
 import com.example.utilsgather.list_guide.GuideSettings
 
 class CustomJuejinNewkiActivity : AppCompatActivity() {
-    lateinit var btnSetProgress: Button
+    lateinit var btnSetProgress2: Button
     lateinit var myCircleProgressView: MyCircleProgressView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_juejin_newki)
 
-        btnSetProgress = findViewById(R.id.btn_set_progress)
+        btnSetProgress2 = findViewById(R.id.btn_set_progress_2)
         myCircleProgressView = findViewById(R.id.mcpv)
 
-        btnSetProgress.setOnClickListener {
-            myCircleProgressView.setupValue(0.2f, 0.8f)
+        btnSetProgress2.setOnClickListener {
+            val vvv : MyCircleProgressView= findViewById(R.id.mcpv2)
+            vvv.setupValue(0f, 1f)
         }
         GuideSettings.setHorizontal(
             findViewById(R.id.rv_horizontal),
