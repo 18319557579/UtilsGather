1eba276178e778df931de0f41bee20e41b5c0373
ALittleDaisy <1152594956@qq.com>
2024-10-30 14:46:47 +0800

    状态栏-内容是否延伸到状态栏

--- app/src/main/java/com/example/utilsuser/immersion/CompositeActivity.kt
+++ app/src/main/java/com/example/utilsuser/immersion/CompositeActivity.kt
@@ -94,17 +94,44 @@ class CompositeActivity : BaseTabViewpagerActivity() {
             ))
             )
             add(
-                Pair("状态栏", ShowFragment.newInstance(
+                Pair("状态栏-内容是否延伸到状态栏", ShowFragment.newInstance(
                 arrayOf(
-                    InnerItemEntity("功能1") { TODO("Not yet implemented") },
-                    InnerItemEntity("功能1") { TODO("Not yet implemented") },
-                    InnerItemEntity("功能1") { TODO("Not yet implemented") },
-                    InnerItemEntity("功能1") { TODO("Not yet implemented") },
+                    InnerItemEntity("（Android11过时）setSystemUiVisibility() 与 WTFs 实现 ---------------------") {  },
+                    InnerItemEntity("将内容延伸到状态栏") {
+                        // 将内容延伸到状态栏
+                        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    },
+                    InnerItemEntity("取消内容延伸到状态栏") {
+                        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility and
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN.inv()
+                    },
+
+                    InnerItemEntity("（Android11开始） ---------------------") {  },
+                    InnerItemEntity("将内容延伸到状态栏") {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            window.setDecorFitsSystemWindows(false)  // 将内容延伸到了状态栏和导航栏
+                        }
+                    },
+                    InnerItemEntity("取消内容延伸到状态栏") {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            window.setDecorFitsSystemWindows(true)  // 内容从状态栏和导航栏出来了
+                        }
+                    },
+
+                    InnerItemEntity("（AndroidX兼容库） ---------------------") {  },
+                    InnerItemEntity("将内容延伸到状态栏") {
+                        WindowCompat.setDecorFitsSystemWindows(window, false) // 让内容延伸到系统窗口边界
+                    },
+                    InnerItemEntity("取消内容延伸到状态栏") {
+                        WindowCompat.setDecorFitsSystemWindows(window, true)  // 内容从状态栏和导航栏出来了
+                    },
                 )
             ))
             )
             add(
-                Pair("状态栏", ShowFragment.newInstance(R.layout.activity_email,
+                Pair("状态栏", ShowFragment.newInstance(
                 arrayOf(
                     InnerItemEntity("功能1") { TODO("Not yet implemented") },
                     InnerItemEntity("功能1") { TODO("Not yet implemented") },
