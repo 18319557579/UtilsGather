0ae5878886f883cc070647bb160c8149764e8989
ALittleDaisy <1152594956@qq.com>
2025-01-07 14:45:32 +0800

    1.切换当前应用的系统语言 2.打印Application层面的String

--- app/src/main/java/com/example/utilsuser/my_locale/MyLocaleActivity.kt
+++ app/src/main/java/com/example/utilsuser/my_locale/MyLocaleActivity.kt
@@ -1,8 +1,10 @@
 package com.example.utilsuser.my_locale
 
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.ListView
+import com.example.utilsgather.context.ApplicationGlobal
 import com.example.utilsgather.lifecycle_callback.LifecycleLogActivity
 import com.example.utilsgather.list_guide.GuideItemEntity
 import com.example.utilsgather.list_guide.GuideSettings
@@ -24,7 +26,7 @@ class MyLocaleActivity : LifecycleLogActivity() {
         val listView = findViewById<ListView>(R.id.lv_launcher)
         GuideSettings.set(
             listView, arrayOf<GuideItemEntity>(
-                GuideItemEntity("当前系统的Locale"){
+                GuideItemEntity("系统的Locale"){
                     val currentLocale = Locale.getDefault()
 
                     // 输出当前应用的 Locale 信息
@@ -34,8 +36,8 @@ class MyLocaleActivity : LifecycleLogActivity() {
 
                     LogUtil.d("Locale: $currentLocale")
                     LogUtil.d("language: $language")
-                    LogUtil.d("Locale: $country")
-                    LogUtil.d("Locale: $displayName")
+                    LogUtil.d("country: $country")
+                    LogUtil.d("displayName: $displayName")
                 },
                 GuideItemEntity("当前应用的Locale"){
                     // 获取当前应用的 Locale
@@ -48,15 +50,55 @@ class MyLocaleActivity : LifecycleLogActivity() {
 
                     LogUtil.d("Locale: $currentLocale")
                     LogUtil.d("language: $language")
-                    LogUtil.d("Locale: $country")
-                    LogUtil.d("Locale: $displayName")
+                    LogUtil.d("country: $country")
+                    LogUtil.d("displayName: $displayName")
                 },
-                GuideItemEntity("打印特定的字符串"){
+                GuideItemEntity("打印特定的字符串-当前Activity层面"){
                     val specificStr = getString(R.string.specific_str)
                     LogUtil.d("特定的字符串: $specificStr")
                 },
-
+                GuideItemEntity("打印特定的字符串-Application层面"){
+                    val specificStr = ApplicationGlobal.getInstance().getString(R.string.specific_str)
+                    LogUtil.d("特定的字符串: $specificStr")
+                },
+                GuideItemEntity("switch zh"){
+                    setLocale("zh")
+//                    setLocale(Locale.CHINA)
+                },
+                GuideItemEntity("switch en"){
+                    setLocale("en")
+//                    setLocale(Locale.US)
+                },
+                GuideItemEntity("switch pt"){
+                    setLocale("pt")
+//                    setLocale(Locale.P)
+                },
             )
         )
+    }
+
+    // 设置应用的 Locale
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale) // 更新 Locale 为默认语言
+
+        // 更新配置文件
+        val config: Configuration = Configuration()
+        config.locale = locale
+
+        // 更新资源，刷新 UI
+        resources.updateConfiguration(config, resources.displayMetrics)
+    }
+
+    // 设置应用的 Locale
+    private fun setLocale(locale: Locale) {
+        Locale.setDefault(locale) // 更新 Locale 为默认语言
+
+        // 更新配置文件
+        val config: Configuration = Configuration()
+        config.locale = locale
+
+        // 更新资源，刷新 UI
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 }
